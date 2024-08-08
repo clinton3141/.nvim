ff691c0c845c7c5bc0e9f3ac3fdd4755387e232d
Clinton Montague <clinton3141@gmail.com>
2024-08-08 19:11:34 +0100

    Line number settings

--- lua/options.lua
+++ lua/options.lua
@@ -2,4 +2,9 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
+
+-- setting both enables "hybrid" line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+
 vim.g.mapleader = " "
