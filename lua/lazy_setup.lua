5ed69605f4fe8e17ab8a54210073d1a9f19a2f74
Clinton Montague <clinton3141@gmail.com>
2024-08-09 17:01:15 +0100

    lock version of lazy

--- lua/lazy_setup.lua
+++ lua/lazy_setup.lua
@@ -1,7 +1,7 @@
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=v11.4.1', lazyrepo, lazypath }
   if vim.v.shell_error ~= 0 then
     error('Error cloning lazy.nvim:\n' .. out)
   end
