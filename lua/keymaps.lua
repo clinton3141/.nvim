a2bad5751b3bb902baf7d395584be8026eadeea9
Clinton Montague <clinton3141@gmail.com>
2024-08-09 22:33:54 +0100

    keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -3,9 +3,9 @@ local wk = require("which-key")
 -- Harpoon
 local harpoon = require("harpoon")
 wk.add({
-    { "<leader>h", group = "[H]arpoon" },
-    { "<leader>ha", function() harpoon:list():add() end, desc= "[A]dd Buffer" },
-    { "<leader>hl", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, desc= "[L]ist Buffers" },
+    { "<leader>h", group = "[h]arpoon" },
+    { "<leader>ha", function() harpoon:list():add() end, desc= "[a]dd Buffer" },
+    { "<leader>hl", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, desc= "[l]ist Buffers" },
     { "<leader>1", function() harpoon:list():select(1) end, desc= "which_key_ignore" },
     { "<leader>2", function() harpoon:list():select(2) end, desc= "which_key_ignore" },
     { "<leader>3", function() harpoon:list():select(3) end, desc= "which_key_ignore" },
@@ -20,16 +20,45 @@ wk.add({
 -- LSP
 wk.add({
     { "K", vim.lsp.buf.hover },
-    { "gd", vim.lsp.buf.definition },
-    { "gr", vim.lsp.buf.references },
-    { "<leader>ca", vim.lsp.buf.code_action, desc="[C]ode [Action]" },
+    { "gd", vim.lsp.buf.definition, desc="go [d]efinition" },
+    { "gr", vim.lsp.buf.references, desc="go [r]eferences" },
+    { "<leader>ca", vim.lsp.buf.code_action, desc="[c]ode [action]" },
 });
 
 -- Telescope
 local telescope = require("telescope.builtin")
 wk.add({
-    { "<leader>f", group = "Telescope" },
-    { "<leader>ff", telescope.find_files, desc="Find files" },
-    { "<leader>fg", telescope.live_grep, desc="Live [G]rep" }
+    { "<leader>s", group = "Tele[s]cope" },
+
+    { "<leader>s ", telescope.buffers, desc="Buffers" },
+
+    { "<leader>s/",  group = "Text search" },
+    { "<leader>s//", function ()
+        telescope.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown {
+            previewer = false
+        })
+    end, desc="[/] Fuzzy" },
+    { "<leader>s/g", function()
+        telescope.live_grep {
+            grep_open_files = true,
+            prompt_title = 'grep open files'
+        }
+    end, desc="[g]rep open" },
+    { "<leader>s/G", function()
+        telescope.live_grep {
+            prompt_title = 'grep all files'
+        }
+    end, desc="[G]rep all" },
+
+    { "<leader>sg", group="[s]earch [g]it", icon=require("nvim-web-devicons").get_icon("git", "git") },
+    { "<leader>sgb", telescope.git_branches, desc="[b]ranches" },
+    { "<leader>sgc", telescope.git_commits, desc="[c]ommits" },
+    { "<leader>sgC", telescope.git_bcommits, desc="buffer [C]ommits" },
+
+    { "<leader>sf", telescope.find_files, desc="Find files" },
+    { "<leader>sh", telescope.help_tags, desc="[h]elp" },
+    { "<leader>sk", telescope.keymaps, desc="[k]eymaps" },
+    { "<leader>sr", telescope.resume, desc="[r]esume" },
+    { "<leader>st", telescope.builtin, desc="[t]elescope" }
 })
 
