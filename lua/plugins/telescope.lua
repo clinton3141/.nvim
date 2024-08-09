1c401ec6d1332a9834f5be84854ee9e5da8bebdc
Clinton Montague <clinton3141@gmail.com>
2024-08-09 21:26:27 +0100

    Add telescope-ui-select

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,6 +2,16 @@ return {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.8",
     dependencies = {
-        "nvim-lua/plenary.nvim"
-    }
+        "nvim-lua/plenary.nvim",
+        "nvim-telescope/telescope-ui-select.nvim"
+    },
+    extensions = {
+        ["ui-select"] = {
+            require("telescope.themes").get_dropdown {}
+        }
+    },
+    config = function ()
+        local telescope = require("telescope")
+        telescope.load_extension("ui-select")
+    end
 }
