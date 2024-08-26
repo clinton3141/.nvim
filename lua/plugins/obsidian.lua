95e4102210c544129213cbec8b7df1a8cd51a42b
Clinton Montague <clinton3141@gmail.com>
2024-08-26 22:42:52 +0100

    Obsidian updates

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -30,9 +30,32 @@ return {
     opts = {
         daily_notes = {
             folder = "Daily notes",
+            template = "daily.md",
         },
         notes_subdir = "inbox",
+        new_notes_location = "notes_subdir",
         workspaces = workspaces,
+        templates = {
+            folder = "templates",
+        },
+        note_frontmatter_func = function(note)
+          -- Add the title of the note as an alias.
+          if note.title then
+            note:add_alias(note.title)
+          end
+
+          local out = { id = note.id, aliases = note.aliases, tags = note.tags }
+
+          -- `note.metadata` contains any manually added fields in the frontmatter.
+          -- So here we just make sure those fields are kept in the frontmatter.
+          if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+            for k, v in pairs(note.metadata) do
+              out[k] = v
+            end
+          end
+
+          return out
+        end,
     }
 }
 
